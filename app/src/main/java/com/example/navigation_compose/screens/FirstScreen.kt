d844acdc36194e2e72485a59e932396afc37d478
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-15 17:15:02 -0400

    Primera pantalla creada.

---
+++ app/src/main/java/com/example/navigation_compose/screens/FirstScreen.kt
@@ -0,0 +1,41 @@
+package com.example.navigation_compose.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FirstScreen(){
+    Scaffold(){
+        BodyContent()
+    }
+}
+
+@Composable
+fun BodyContent(){
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Hola, Prueba de nav.")
+        Button(onClick={/*TODO*/}){
+            Text(text = "NAVEGA")
+        }
+        
+    }
+}
+@Preview
+@Composable
+fun Vista(){
+    FirstScreen()
+}
